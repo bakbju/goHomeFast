ef1a7562b730dd50a0940baae8277c10da6c03b7
bakbju <bakbju@gmail.com>
2018-02-20 16:23:21 +0900

    avz

--- src/main/java/com/bakbj/redirect/RedirectPortToHttps.java
+++ src/main/java/com/bakbj/redirect/RedirectPortToHttps.java
@@ -45,7 +45,7 @@ public class RedirectPortToHttps {
     private Connector initiateHttpConnector() {
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
         connector.setScheme("http");
-        connector.setPort(80);
+        connector.setPort(8080);
         connector.setSecure(false);
         connector.setRedirectPort(8443);
         return connector;
