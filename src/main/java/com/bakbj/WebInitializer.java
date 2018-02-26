cafbe91dfcc05c51c836fd26e9cefd916cc7290a
bakbju <bakbju@gmail.com>
2018-02-26 14:02:44 +0900

    adsfzxcvqer

---
+++ src/main/java/com/bakbj/WebInitializer.java
@@ -0,0 +1,12 @@
+package com.bakbj;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class WebInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GoHomeFastApplication.class);
+    }
+}
