37f8cf09780371112bdcc7c0aa5bfc0fe6170b97
bakbju <bakbju@gmail.com>
2018-02-27 18:30:42 +0900

    version15

--- src/main/java/com/bakbj/GoHomeFastApplication.java
+++ src/main/java/com/bakbj/GoHomeFastApplication.java
@@ -1,15 +1,8 @@
 package com.bakbj;
 
-import org.apache.tomcat.util.descriptor.web.SecurityCollection;
-import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
-import org.springframework.boot.context.embedded.jetty.JettyEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 import javax.naming.Context;
 
